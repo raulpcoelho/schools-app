6d5249f2856973f188977834e7d5466e5e83bdd6
Raul Coelho <raulpereira5@gmail.com>
2023-12-08 01:37:52 -0300

    schools list tab added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../services/api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  public schools: any;
+  
+  constructor(private apiService: ApiService) { }
 
-  constructor() {}
-
+  ngOnInit() {
+    this.apiService.getSchools().subscribe({
+      next: data => this.schools = data,
+      error: err => console.error({"Error": err})
+    });
+  }
 }
